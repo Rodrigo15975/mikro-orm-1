178835043538ad5cf5dd4a7949ce19630dbca1a4
rodrigo15975 <javierluis15975@hotmail.com>
2025-04-02 23:55:38 -0500

    update error.ts

--- src/common/error.ts
+++ src/common/error.ts
@@ -7,13 +7,11 @@ type Props = {
 export function CustomError(error: unknown): Props {
   if (typeof error === 'object' && error !== null) {
     const { message, stack, status } = error as Record<string, unknown>
-
     return {
       message: typeof message === 'string' ? message : JSON.stringify(error),
       stack: typeof stack === 'string' ? stack : undefined,
       status: typeof status === 'number' ? status : undefined,
     }
   }
-
   return { message: JSON.stringify(error) }
 }
